62c0faed9d5f4544d31873c584a748bf97840ceb
vantasm <khoeric1@yahoo.co.id>
2022-01-07 15:53:33 +0700

    tampilan upload

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,15 +1,20 @@
 <style>
 input#file {
-  display: inline-block;
-  width: 20%;
-  padding: 120px 0 0 0;
-  height: 100px;
-  overflow: hidden;
-  -webkit-box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  box-sizing: border-box;
-  border-radius: 20px;
-  background-size: 60px 60px;
+    display: inline-block;
+    width: 20%;
+    padding: 120px 0 0 0;
+    height: 100px;
+    overflow: hidden;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    border-radius: 20px;
+    background-size: 60px 60px;
+}
+
+img{
+    width: 20%;
+    height: 50%;
 }
 </style>
 
@@ -19,14 +24,22 @@ input#file {
 
 
 <div class="container">
-    <div class="row justify-content-center mb-3 pb-3">
+    <div class="row justify-content-center">
         <h2 class="mb-5" style="padding-bottom: ">Upload Evidence of Transfer</h2>
+    </div>
+</div>
+
+
+{{-- Display evidence of transfer --}}
+<div class="container">
+    <div class="row justify-content-center mb-3 pb-3">
+        <img src="/images/bukti transfer.jpg" alt="">
     </div>
 </div>
 
 {{-- Upload --}}
 <div class="container">
-    <div class="row justify-content-center mb-3 pb-3">
+    <div class="row justify-content-center">
         <div class="col-md-12 heading-section text-center ftco-animate">
             <form action="" method="post" enctype="multipart/form-data">             
                 <button  class="btn btn-danger py-3 px-5 " onclick="document.getElementById('getFile').click()">Upload</button>
@@ -46,8 +59,5 @@ input#file {
         </div>
     </div>
 </div>
-
 </section>
-
-
 @endsection
