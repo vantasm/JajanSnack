d8d2d3a9c22e791f6595ab8818fffaad0668f904
ArjunaAcchaDipa <62648000+ArjunaAcchaDipa@users.noreply.github.com>
2022-01-09 21:32:32 +0700

    Update: display snack template

--- resources/views/layouts/include/display_snack.blade.php
+++ resources/views/layouts/include/display_snack.blade.php
@@ -20,19 +20,21 @@
                     </p>
                 </div>
             </div>
-            <div class="bottom-area d-flex px-3">
-                <div class="m-auto d-flex">
-                    <a href="#" class="add-to-cart d-flex justify-content-center align-items-center text-center">
-                        <span><i class="fas fa-cart-plus"></i></span>
-                    </a>
-                    <a href="#" class="buy-now d-flex justify-content-center align-items-center mx-1">
-                        <span><i class="fas fa-shopping-cart"></i></span>
-                    </a>
-                    <a href="#" class="heart d-flex justify-content-center align-items-center ">
-                        <span><i class="fas fa-heart"></i></span>
-                    </a>
+            @if(Auth::user())
+                <div class="bottom-area d-flex px-3">
+                    <div class="m-auto d-flex">
+                        <a href="#" class="add-to-cart d-flex justify-content-center align-items-center text-center">
+                            <span><i class="fas fa-cart-plus"></i></span>
+                        </a>
+                        <a href="#" class="buy-now d-flex justify-content-center align-items-center mx-1">
+                            <span><i class="fas fa-shopping-cart"></i></span>
+                        </a>
+                        <a href="#" class="heart d-flex justify-content-center align-items-center ">
+                            <span><i class="fas fa-heart"></i></span>
+                        </a>
+                    </div>
                 </div>
-            </div>
+            @endif
         </div>
     </div>
 </div>
